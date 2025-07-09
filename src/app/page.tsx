07d2be3f9c28a5021028ade536cea243450c849d
PAOO2711 <pedroabril2a@gmail.com>
2025-07-09 11:47:52 +0200

    Button and form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import Image from "next/image";
+import Image from "next/image"
 import { Button } from '@/components/ui/button'
 import { SendIcon } from "lucide-react"
+import FormDemo from "@/app/form"
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>
-        <SendIcon /> Send
-      </Button>
-    </main>
+    <div className="grid grid-cols-2 ">
+      <div className="flex min-h-screen items-center justify-center">
+        <FormDemo>
+          
+        </FormDemo>
+      </div>
+
+      <div className="flex min-h-screen items-center justify-center">
+        <Button>Start</Button>
+      </div>
+    </div>
   );
 }
+
